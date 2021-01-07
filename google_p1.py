abb76f98d41c65af6ba729a31fd0576f2fa5e141
koc <koc@kofficecom.co.kr>
2021-01-07 17:51:56 +0900

    Jan 07 Exercise

--- google_p1.py
+++ google_p1.py
@@ -9,19 +9,10 @@ elem = driver.find_element_by_name("q")
 elem.send_keys("조코딩")
 elem.send_keys(Keys.RETURN)
 
-SCROLL_PAUSE_TIME = 0.5
-
-# Get scroll height
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
-count = 1
-for image in images:
-    image.click()
-    time.sleep(3)
-    imgUrl = driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')
-    urllib.request.urlretrieve(imgUrl, "test" + str(count) + ".jpg")
-    count = count + 1
+driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
+time.sleep(3)
+imgUrl = driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')
+urllib.request.urlretrieve(imgUrl, "test.jpg")
 
 
 
