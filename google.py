6f9368cd6d5d925f74b7c6d70f397760a5acf49d
koc <koc@kofficecom.co.kr>
2021-01-07 13:00:33 +0900

    new

---
+++ google.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("조코딩")
+elem.send_keys(Keys.RETURN)
+
+
+# assert "Python" in driver.title
+# elem = driver.find_element_by_name("q")
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+# assert "No results found." not in driver.page_source
+# driver.close()
